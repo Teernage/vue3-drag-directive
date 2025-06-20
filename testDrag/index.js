7a3f7d7f7ce6aa6b395334da8a62cfbca7af750f
xuzhenxin <xuzhenxin@oray.com>
2025-06-20 18:26:46 +0800

    feat: 删除无用的文件夹

--- testDrag/index.js
+++
@@ -1,7 +0,0 @@
-// test-import.mjs
-import { vDragList, registerDragList } from '../dist/index.esm.js';
-import DragListPlugin from '../dist/index.esm.js';
-
-console.log('vDragList:', typeof vDragList);
-console.log('registerDragList:', typeof registerDragList);
-console.log('DragListPlugin:', typeof DragListPlugin);
